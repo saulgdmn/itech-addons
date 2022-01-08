c0f1778d3174bf64de51f5525475360d372800f4
Your Name <you@example.com>
2022-01-08 17:18:19 +0200

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace NureState
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 109);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(116, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(24, 363);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(96, 32);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Location = new System.Drawing.Point(24, 325);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(96, 32);
+            this.btnFetch.TabIndex = 3;
+            this.btnFetch.Text = "Fetch";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            this.btnFetch.Click += new System.EventHandler(this.btnFetch_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 136);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(116, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Compare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {this.colName, this.colPos});
+            this.dataGridView.Location = new System.Drawing.Point(210, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(990, 464);
+            this.dataGridView.TabIndex = 6;
+            // 
+            // colName
+            // 
+            this.colName.HeaderText = "Name";
+            this.colName.Name = "colName";
+            this.colName.Width = 66;
+            // 
+            // colPos
+            // 
+            this.colPos.HeaderText = "Position";
+            this.colPos.Name = "colPos";
+            this.colPos.Width = 76;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1212, 488);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.btnFetch);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPos;
+
+        private System.Windows.Forms.DataGridView dataGridView;
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.ComboBox comboBox2;
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Data.SQLite;
+using System.Globalization;
+
+namespace NureState
+{
+    public partial class Form1 : Form
+    {
+        private String connString = "Data Source=nure.db";
+        private String nureUrl = "https://dec.nure.ua/uk-about-department/uk-teaching-staff/";
+        public Form1()
+        {
+            InitializeComponent();
+            CreateTable();
+            var dates = PullFetchedDates();
+            foreach (var d in dates)
+            {
+                comboBox1.Items.Add(d.ToString("yyyy-MM-dd HH:mm:ss"));
+                comboBox2.Items.Add(d.ToString("yyyy-MM-dd HH:mm:ss"));
+            }
+        }
+
+        private Dictionary<String, String> ParseFile(String filename)
+        {
+            Dictionary<String, String> results = new Dictionary<String, String>();
+            HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.Load(filename, Encoding.UTF8);
+            if (htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Count() == 0)
+            {
+                foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//td[@class]"))
+                {
+                    var nameNode = node.SelectSingleNode("strong");
+                    var positionNode = node.SelectSingleNode("em");
+                    if (nameNode == null || positionNode == null)
+                        continue;
+                    String name = nameNode.InnerText.
+                        Replace("'", "''").
+                        Replace("\n", " ");
+                    String position =positionNode.InnerText.
+                        Replace("\n", " "); 
+                    results.Add(name, position);
+                }
+            }
+
+            return results;
+        }
+
+        private (String, DateTime) FetchUrl(String url)
+        {
+            DateTime dt = DateTime.Now;
+            String filename = $".\\{dt.ToString("yyyy-MM-dd HH-mm-ss")}.html";
+            using(WebClient client = new WebClient()) {
+                client.DownloadFile(url, filename);
+            }
+
+            return (filename, dt);
+        }
+
+        private void CreateTable()
+        {
+            using (var connection = new SQLiteConnection(connString))
+            {
+                connection.Open();
+ 
+                SQLiteCommand command = new SQLiteCommand();
+                command.Connection = connection;
+                command.CommandText = $"CREATE TABLE IF NOT EXISTS NureState(" +
+                                      $"Name TEXT NOT NULL," +
+                                      $"Position TEXT NOT NULL," +
+                                      $"Fetched DATETIME NOT NULL)";
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private List<DateTime> PullFetchedDates()
+        {
+            List<DateTime> dates = new List<DateTime>();
+            using (SQLiteConnection connect = new SQLiteConnection(connString)){
+                connect.Open();
+                using (SQLiteCommand cmd = connect.CreateCommand()){
+                    cmd.CommandText = @"SELECT DISTINCT Fetched FROM NureState";
+                    cmd.CommandType = CommandType.Text;
+                    SQLiteDataReader r = cmd.ExecuteReader();
+                    while (r.Read()){
+                        dates.Add(DateTime.Parse(r["Fetched"].ToString()));
+                    }
+                }
+            }
+
+            return dates;
+        }
+        
+        private Dictionary<String, String> PullData(DateTime dt)
+        {
+            Dictionary<String, String> results = new Dictionary<String, String>();
+            using (SQLiteConnection connect = new SQLiteConnection(connString)){
+                connect.Open();
+                using (SQLiteCommand cmd = connect.CreateCommand()){
+                    cmd.CommandText = $"SELECT Name, Position " +
+                                      $"FROM NureState " +
+                                      $"WHERE Fetched = '{dt.ToString("yyyy-MM-dd HH:mm:ss")}'";
+                    cmd.CommandType = CommandType.Text;
+                    SQLiteDataReader r = cmd.ExecuteReader();
+                    while (r.Read()){
+                        results.Add(Convert.ToString(r["Name"]),Convert.ToString(r["Position"]));
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        private void PushData(Dictionary<String, String> data, DateTime dt)
+        {
+            using (var connection = new SQLiteConnection("Data Source=nure.db"))
+            {
+                connection.Open();
+                foreach (KeyValuePair<String, String> d in data)
+                {
+                    SQLiteCommand command = new SQLiteCommand();
+                    command.Connection = connection;
+                    command.CommandText = $"INSERT INTO NureState (Name, Position, Fetched)" +
+                                          $"VALUES ('{d.Key}', '{d.Value}', '{dt.ToString("yyyy-MM-dd HH:mm:ss")}')";
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void btnFetch_Click(object sender, EventArgs e)
+        {
+            var tuple = FetchUrl(nureUrl);
+            var results = ParseFile(tuple.Item1);
+            PushData(results, tuple.Item2);
+            comboBox1.Items.Add(tuple.Item2.ToString("yyyy-MM-dd HH:mm:ss"));
+            comboBox2.Items.Add(tuple.Item2.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = ".\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    String filename = openFileDialog.FileName;
+                    var data = ParseFile(filename);
+                    String date = filename.Split('\\').Last().Split('.').First();
+                    DateTime dt = DateTime.ParseExact(date, "yyyy-MM-dd HH-mm-ss", CultureInfo.InvariantCulture);
+                    PushData(data, dt);
+                    comboBox1.Items.Add(dt.ToString("yyyy-MM-dd HH:mm:ss"));
+                    comboBox2.Items.Add(dt.ToString("yyyy-MM-dd HH:mm:ss"));
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGrid_Navigate(object sender, NavigateEventArgs ne)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+                return;
+
+            var results1 = PullData(DateTime.ParseExact(
+                comboBox1.SelectedItem.ToString(), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture));
+            var results2 = PullData(DateTime.ParseExact(
+                comboBox2.SelectedItem.ToString(), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture));
+            dataGridView.Rows.Clear();
+            foreach (var pair in results1)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.Cells.Add(new DataGridViewTextBoxCell());
+                row.Cells.Add(new DataGridViewTextBoxCell());
+                row.Cells[0].Value = pair.Key;
+                row.Cells[1].Value = pair.Value;
+                if (results2.ContainsKey(pair.Key))
+                {
+                    if (results2[pair.Key] != pair.Value)
+                    {
+                        row.Cells[1].Style.BackColor = Color.Blue;
+                    }
+                }
+                else
+                    row.DefaultCellStyle.BackColor = Color.Green;
+                
+                dataGridView.Rows.Add(row);
+            }
+
+            foreach (var pair in results2)
+            {
+                if (results1.ContainsKey(pair.Key))
+                    continue;
+                
+                DataGridViewRow row = new DataGridViewRow();
+                row.Cells.Add(new DataGridViewTextBoxCell());
+                row.Cells.Add(new DataGridViewTextBoxCell());
+                row.Cells[0].Value  = pair.Key;
+                row.Cells[1].Value = pair.Value;
+                row.DefaultCellStyle.BackColor = Color.Red;
+                dataGridView.Rows.Add(row);
+            }
+        }
+    }
+}
